965e4edf9c35a2eb485de5731d97239ede68ddc2
JorisVanRiel <you@example.com>
2021-04-25 03:40:58 +0200

    Used serializeField attribute on Camera offset

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -7,7 +7,7 @@ public class FollowPlayer : MonoBehaviour
 
     public GameObject Player;
 
-    private Vector3 offset = new Vector3(0, 5, -7);
+    [SerializeField] private Vector3 offset = new Vector3(0, 5, -7);
     // Start is called before the first frame update
     void Start()
     {
