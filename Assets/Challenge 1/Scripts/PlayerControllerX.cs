0eff6d7b22b9c2c44099859c96ae8853dcb46586
JorisVanRiel <you@example.com>
2021-04-25 03:38:57 +0200

    Added git to finished prototype

---
+++ Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public float speed;
+    public float rotationSpeed;
+    public float verticalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        this.Rotate();
+        this.Move();
+    }
+
+    private void Rotate()
+    {
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Rotate(Vector3.right * rotationSpeed * verticalInput);
+    }
+
+    private void Move()
+    {
+        this.AjustSpeed();
+        transform.Translate(Vector3.forward * Time.deltaTime * this.speed);
+    }
+
+    private void AjustSpeed()
+    {
+        if (Input.GetKey(KeyCode.Q)) this.speed++;
+        if (Input.GetKey(KeyCode.E)) this.speed--;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed = 20f;
+    public float TurnSpeed;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        Move();
+        Turn();
+    }
+
+    private void Move()
+    {
+        float inputSpeed = Input.GetAxis("Vertical");
+        this.transform.Translate(Vector3.forward * Time.deltaTime * this.Speed * inputSpeed);
+    }
+
+    private void Turn()
+    {
+        float inputSpeed = Input.GetAxis("Horizontal");
+        this.transform.Rotate(Vector3.up,  Time.deltaTime * this.TurnSpeed * inputSpeed);
+    }
+
+}
